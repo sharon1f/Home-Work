f41d4a09309236ce54f9700dc964a21c9b1cf5cc
Sharon <sharonfaduck@gmail.com>
2018-01-23 10:24:42 +0200

    TEST

--- seleuim.py
+++ seleuim.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://www.ebay.com")
+driver.get("http://www.eBay.com")
 driver.find_element_by_id("gh-ac").send_keys("htc10")
 driver.find_element_by_id("gh-ac").send_keys(Keys.ENTER)
